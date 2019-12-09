20bc7cbe92e97c133388f7d093143a87a3b191f1
Daniel Chow <DChow@spartaglobal.com>
2019-12-09 22:39:13 +0000

    edited method to create txt file with info on user

--- names.py
+++ names.py
@@ -1,16 +1,26 @@
 from new_users_class import *
 
-def open_print_close(file_name):
-    try:
-        with open(file_name, 'r') as file:
-            lines = file.readlines()
-            for line in lines:
-                print(line.strip('\n'))
-    except FileNotFoundError as error:
-        print('Check file name &/or path - File cannot be found')
-        print(error)
-    finally:
-        print('Program closing - execution done!')
+# def open_print_close(file_name):
+#     try:
+#         with open(file_name, 'r') as file:
+#             lines = file.readlines()
+#             for line in lines:
+#                 print(line.strip('\n'))
+#     except FileNotFoundError as error:
+#         print('Check file name &/or path - File cannot be found')
+#         print(error)
+#     finally:
+#         print('Program closing - execution done!')
 
 # open_print_close('names.txt')
 
+new_users = []
+
+opened_file = open('names.txt', 'r')
+lines = opened_file.readlines()
+for line in lines:
+    new_users.append(NewUsers(line))
+print(new_users)
+opened_file.close()
+
+new_users[1].output_text_file('info1.txt', 22, 'red')
--- new_users_class.py
+++ new_users_class.py
@@ -3,10 +3,16 @@ class NewUsers():
     def __init__(self, name):
         self.name = name
 
-    def output_text_file(self, file_name, item):
+    def output_text_file(self, file_name, age, fav_colour):
         try:
             with open(file_name, 'w+') as file_to_create:
-                file_to_create.write(f'The name of this user is {self.name} ')
+                file_to_create.write(f"The name of this user is {self.name}"
+                                     f"The users' age is {age} "
+                                     f"and their favourite colour is {fav_colour}.")
+        # except TypeError as error:
+        #     print('Ensure you have inputted all arguments')
+        #     print(error)
+
         finally:
             print('Program closing - execution done!')
 
